36b0176ccea47b97e7fef52076cbc9daef91a100
dmitryguba <dmitry.huba@gmail.com>
2018-05-03 18:48:11 +0300

    first homeWork

--- src/arrayMethods.js
+++ src/arrayMethods.js
@@ -1,5 +1,40 @@
-window.every = function () {};
-window.some = function () {};
-window.forEach = function () {};
-window.filter = function () {};
-window.map = function () {};
+window.every = function (array, callback) {
+	var isTrue = true;
+	for(var i =0; i<array.length; i++){
+		if(!callback(array[i], i, array)){
+			isTrue=false;
+			break;
+		}
+	}
+	return isTrue;
+};
+window.some = function (array, callback) {
+	var isTrue = false;
+	for(var i =0; i<array.length; i++){
+		if(callback(array[i], i, array)){
+			isTrue = true;
+			break;
+		}
+	}
+	return isTrue;
+};
+window.forEach = function (array, callback) {
+	for(var i =0; i< array.length; i++){
+		callback(array[i], i, array);
+	}
+};
+window.filter = function (array, callback) {
+	var newArray = [];
+	for(var i =0; i<array.length; i++){
+		if(callback(array[i], i, array))
+		newArray.push(array[i]);
+	}
+	return newArray;
+};
+window.map = function (array, callback) {
+		var newArray = [];
+		for(var i =0; i<array.length; i++){
+			newArray.push(callback(array[i], i, array));
+		}
+		return newArray;
+};
